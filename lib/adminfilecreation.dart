f16a545a01f6bb0a50d499bad81fd38428f857fb
UNKNOWN74056 <hamza74056@gmail.com>
2024-11-14 09:11:48 +0500

    add admin file

---
+++ lib/adminfilecreation.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Adminfilecreation extends StatelessWidget {
+  const Adminfilecreation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
